78f735f85c30a7207e9ca068c361f7d93da1d753
luochen <969488647@qq.com>
2025-05-18 15:06:08 +0800

    Site updated: 2025-05-18 15:06:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/18/hello-world/"];function toRandomPost(){
+var posts=["2025/05/18/hello-world/","2025/05/18/hello-world copy/","2025/05/18/hello-world copy 2/","2025/05/18/hello-world copy 3/","2025/05/18/hello-world copy 4/","2025/05/18/hello-world copy 5/","2025/05/18/hello-world copy 6/","2025/05/18/hello-world copy 7/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
